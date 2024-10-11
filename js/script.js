3ab530a02f723b60d1d8da760e8a864bbe94752e
rodrigo kakiuchi de souza <rodrigo.kakiuchi2010@gmail.com>
2024-10-11 17:38:54 -0300

    closes #2 - Carregando clientes na tabela a partir de um array

---
+++ js/script.js
@@ -0,0 +1,29 @@
+clients = [
+    {
+        id : 1,
+        name : 'Rodrigo',
+        surname : "Kakiuchi",
+        adress : "Rua Leite Penteado",
+        adressNumber : 999,
+        cep : "18010-050",
+        district : "Centro",
+        city : "Sorocaba",
+        state : "SP"
+    }
+];
+
+addRow(clients)
+
+function addRow(clients){
+    let table = document.getElementById("clientsTable")
+    for(let client of clients){
+        let row = table.insertRow()
+        row.insertCell().outerHTML = `<th>${client.id}</th>`
+        row.insertCell().innerHTML = client.name + " " + client.surname
+        row.insertCell().innerHTML = client.adress + ", " + client.adressNumber
+        row.insertCell().innerHTML = client.cep
+        row.insertCell().innerHTML = client.district
+        row.insertCell().innerHTML = client.city
+        row.insertCell().innerHTML = client.state
+    }
+}
